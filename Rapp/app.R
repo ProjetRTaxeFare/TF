0c863aa99375bbf143bce8ef9c75950d27c867e7
edouard fachot <edouardfachot@edouards-MacBook-Air.local>
2018-12-10 11:54:34 +0100

    App

--- Rapp/app.R
+++ Rapp/app.R
@@ -1,33 +1,35 @@
 library(shiny)
 library(googleway)
+library(ggmap)
+library(placement)
 
-ui <- navbarPage("APP NAME", position = c("static-top"),tabPanel("MAP",
-                                                                 google_mapOutput(outputId = "mapWarsaw"),
-                                                                 textInput(inputId = "origin", label = "Departure point"),
-                                                                 textInput(inputId = "destination", label = "Destination point"),
-                                                                 actionButton(inputId = "getRoute", label = "Get Route")
+ui <- fluidPage(navbarPage("Itinerary", position = c("static-top"),tabPanel("MAP",
+                                                                            google_mapOutput(outputId = "mapNY"),
+                                                                            textInput(inputId = "origin", label = "Departure point"),
+                                                                            textInput(inputId = "destination", label = "Destination point"),
+                                                                            actionButton(inputId = "getRoute", label = "Get Route")
+))
+
 )
-)
+
 
 server <- function(input, output, session) {
   
   map_key <- "AIzaSyDbsN9eAJDG8lD773Omi2UBASPPVAUiiXs"
   api_key <- "AIzaSyDbsN9eAJDG8lD773Omi2UBASPPVAUiiXs"
   
-  output$mapWarsaw <- renderGoogle_map({
+  output$mapNY <- renderGoogle_map({
     google_map(key = map_key, 
-               location = "New York City",
                search_box = TRUE, 
+               location = c(40.7127753,-74.0059728),
                scale_control = TRUE, 
-               height = 1000) %>%
+               height = 1000,
+               zoom=12) %>%
       add_traffic()
   })
-  observeEvent(input$mapWarsaw_click, {
-    click <- input$mapWarsaw_click
-    clat <- click$lat
-    clng <- click$lng
-    
-  })
+  
+  
+  
   
   observeEvent(input$getRoute,{
     
@@ -51,7 +53,7 @@ server <- function(input, output, session) {
     
     df_way$order <- as.character(1:nrow(df_way))
     
-    google_map_update(map_id = "mapWarsaw") %>%
+    google_map_update(map_id = "mapNY") %>%
       clear_traffic() %>%
       clear_polylines() %>%
       clear_markers() %>%
@@ -59,14 +61,16 @@ server <- function(input, output, session) {
       add_polylines(data = df_route,
                     polyline = "route",
                     stroke_colour = "#FF33D6",
-                    stroke_weight = 7,
-                    stroke_opacity = 0.7,
+                    stroke_weight = 5,
+                    stroke_opacity = 0.6,
                     info_window = "New route",
                     load_interval = 100) %>%
       add_markers(data = df_way,
                   info_window = "end_address",
                   label = "order")
   })
+  
+ 
 }
 
 
