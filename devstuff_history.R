bceaea13c140362ea28c8bafd960c23cc56d012e
Simon Tirman <simontirman@Simons-MacBook-Pro.local>
2018-12-12 14:54:37 +0100

    Add new transfo data

--- devstuff_history.R
+++ devstuff_history.R
@@ -31,5 +31,9 @@ file <- system.file("data_model/final_model_result_with_200000_data.csv", packag
 model_df <- read.csv(file, sep = ",")
 usethis::use_data(model_df, overwrite = TRUE)
 
+file <- system.file("data_model/train_transfo2.csv", package = "TFpackage")
+model_df <- read.csv(file, sep = ",")
+usethis::use_data(model_df, overwrite = TRUE)
+
 train_sample <- train[1:10,]
 usethis::use_data(train_sample)
